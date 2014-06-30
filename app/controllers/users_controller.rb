acae13c41e736e3c497ea4f35a79b9dd482f7874
ownadi <ownadi@outlook.com>
2014-06-30 22:42:16 +0200

    User refactoring.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
-  #before_action :set_user
+  before_action :set_user, only: [:show]
 
   def show
-    @user = User.find(params[:id])
   end
 
   def leaderboard
